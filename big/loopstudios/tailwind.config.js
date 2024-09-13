bb953389635e3a7bf9c2439cedffc689def50a16
Rafiul Islam <rafi.cse.ru@gmail.com>
2024-09-13 23:09:29 +0600

    loopstudios: build hero section.

--- big/loopstudios/tailwind.config.js
+++ big/loopstudios/tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    screen: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Josefin Sans", "sans-serif"],
+        alata: ["Alata"],
+      },
+    },
   },
   plugins: [],
 };
